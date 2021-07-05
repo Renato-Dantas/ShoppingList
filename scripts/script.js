986ce8a1fd45e2553422dceceeecf7f4fcaaca9b
Renato Dantas <renatodantas@outlook.com.br>
2021-07-05 17:02:39 -0300

    inserindo funções para listas

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,27 @@
+let item = document.getElementById('searchArea')
+let title = document.getElementById('empty')
+let lista = document.getElementById('items')
+let nItems = document.getElementById('nItems')
+
 function addProduct(){
-    alert('Produto Adicionado a lista')
+    addItemToList()
+    clearTitle()
+    countList()
+}
+
+function addItemToList(){
+    lista.innerHTML += '<li class = "item">'+ item.value + '</li>'
+}
+
+function clearTitle(){
+    title.style.setProperty('display', 'none')
+    item.value = ''  
+}
+
+function countList(){
+    let count = lista
+    for(i=0;i<count.lenght; i++){
+        console.log(i)
+    }
+
 }
