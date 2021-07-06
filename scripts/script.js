d2fb115f8c56e1bb935faa6856dd299d28afaee0
Renato Dantas <renatodantas@outlook.com.br>
2021-07-06 13:17:12 -0300

    incluindo campos de input de preço e calculo do n° de itens

--- scripts/script.js
+++ scripts/script.js
@@ -10,7 +10,7 @@ function addProduct(){
 }
 
 function addItemToList(){
-    lista.innerHTML += '<li class = "item">'+ item.value + '</li>'
+    lista.innerHTML += '<li class = "item">'+ item.value + '</li>' + '<input type= "number" placeholder = "Preço" id = "preçoGeral" class = "precos">'
 }
 
 function clearTitle(){
@@ -19,9 +19,19 @@ function clearTitle(){
 }
 
 function countList(){
-    let count = lista
-    for(i=0;i<count.lenght; i++){
-        console.log(i)
-    }
-
+    const textBase = 'Número de itens na lista: '
+    let count = parseInt(document.querySelectorAll("#items li").length)
+    nItems.innerHTML = textBase + count
 }
+
+//Observações:
+/*
+    No input de preço, o valor some após inserir um novo item
+    Seria interessante mudar a cor do placeholder do input de preço
+    Acrescentar botão para retirar um item
+    Acrescentar checkbox para dar check em um item já comprado
+    Salvar os valores dos itens numa lista e calcular o total
+    Salvar os nomes e valores dos itens COM check em uma lista e calcular o total
+    Salvar os nomes e valores dos itens SEM check em uma lista e calcular o total
+    Ajustar automaticamente a fonte do texto para caber na área do parágrafo
+*/
